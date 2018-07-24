353e278be687aaf148d616db9483bed68f996856
Kim Foukaris <kimfoukaris@outlook.com>
2018-07-24 19:31:04 +0000

    automatically backed up by learn

--- app/workers/songs_worker.rb
+++ app/workers/songs_worker.rb
@@ -3,6 +3,8 @@ class SongsWorker
   include Sidekiq::worker
 
   def perform(songs_file)
-
+    CSV.foreach(params["file"].path, headers: true) do |song|
+      Song.create(title: song[0], artist_name: song[1])
+    end
   end
 end
